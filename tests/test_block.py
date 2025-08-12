83ed53e41f71f528fd80f69308be7a89772ab9cb
william <william_swl@163.com>
2025-08-12 12:21:29 +0800

    test: update template test

--- tests/test_block.py
+++ tests/test_block.py
@@ -7,15 +7,19 @@ def test_parse_template():
     res_ref = {
         "block_nrow": 8,
         "block_ncol": 12,
-        "data_nrow": 7,
+        "data_nrow": 6,
         "data_ncol": 10,
-        "data_rows_list": [1, 2, 3, 4, 5, 6, 7],
+        "data_rows_list": [2, 3, 4, 5, 6, 7],
         "data_cols_list": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
         "tablemeta": {"batch": (0, 0)},
         "rowmeta": {
             "item": {"col": 0, "start": 2, "end": 7},
             "titer": {"col": 11, "start": 2, "end": 7},
         },
-        "colmeta": {"conc": {"row": 0, "start": 2, "end": 10}},
+        "colmeta": {
+            "conc": {"row": 0, "start": 2, "end": 10},
+            "ab": {"row": 1, "start": 1, "end": 10},
+        },
     }
+
     assert res == res_ref
